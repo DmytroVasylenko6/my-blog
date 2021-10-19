09484419b0564a827730614ed9daca71e6524e5e
Dmytro Vasylenko <tredstoun651@uk.net>
2021-10-19 15:10:46 +0300

    fixed routes

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -3,25 +3,13 @@ import { Route, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import authSelectors from '../../redux/auth/auth-selectors';
 import PropTypes from 'prop-types';
-import { CSSTransition } from 'react-transition-group';
 
 export default function PrivateRoute({ children, redirectTo, ...routeProps }) {
   const isAuthenticated = useSelector(authSelectors.getIsAuthenticated);
 
   return (
     <Route {...routeProps}>
-      {({ match }) => {
-        console.log(match);
-        return (
-          <CSSTransition
-            in={match != null}
-            timeout={300}
-            classNames="page"
-            unmountOnExit>
-            {isAuthenticated ? children : <Redirect to={redirectTo} />}
-          </CSSTransition>
-        );
-      }}
+      {isAuthenticated ? children : <Redirect to={redirectTo} />}
     </Route>
   );
 }
